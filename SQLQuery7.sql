b5a4d3b5dba42dc5adc073fc4605e2a990b57182
Joachim <joachim.frances@hotmail.be>
2022-01-06 16:53:12 +0100

    Update SQLQuery7.sql

--- SQLQuery7.sql
+++ SQLQuery7.sql
@@ -1,8 +1,8 @@
 --liste des employes
 --nom et prenom de l'employe
 --nom du chef de service
-SELECT (Emp.LastName + ' ' + Emp.FirstName) AS EmployeeName,
-		(Boss.LastName + ' ' + Boss.FirstName) AS BossName
+SELECT CONCAT(Emp.LastName, ' ', Emp.FirstName) AS EmployeeName,
+		CONCAT(Boss.LastName, ' ', Boss.FirstName) AS BossName --*
 FROM Employees AS Emp
 JOIN Employees AS Boss
 ON Boss.EmployeeID = Emp.ReportsTo;
